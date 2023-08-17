426592720aac0c9f616a5947d174e07641572eda
Nandhitha1301 <142478154+Nandhitha1301@users.noreply.github.com>
2023-08-17 12:17:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,38 +5,30 @@ public class Main {
         System.out.println("Please enter your bank account type \'currentaccount\' \'savingsaccount\' \'loanaccount\'");
         Scanner sc=new Scanner(System.in);
         String ch=sc.next();
-
+        ch.toLowerCase();
         switch(ch)
         {
             case "currentaccount":
-            {
                 current c=new current();
                 c.createAccount();
                 c.InterestRate();
                 c.curracc();
                 break;
-
-            }
             case "savingsaccount":
-            {
                 saving s = new saving();
                 s.createAccount();
                 s.InterestRate();
                 s.savacc();
                 break;
-            }
             case "loanaccount":
-            {
                 loan l =new loan();
                 l.createAccount();
                 l.InterestRate();
                 l.loanacc();
                 break;
-            }
             default:
-            {
                 System.out.println("Wrongly selected!! please enter correctly..");
-            }
+                break;
 
         }
 
